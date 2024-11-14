4129bc24576cc85fd019221283eb259843584534
Nael2311 <nael_96@uma.es>
2024-11-14 17:00:45 +0100

    CAMBIO LIBREARIA "lmic-esp-idf" A LA LIBRERIA "ttn-esp32"

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/nael_/Desktop/TFG/ESP-IDF-Pruebas/PruebaDefinitiva/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/nael_/Desktop/.git")
+set(GIT_DIR "C:/Users/nael_/Desktop/TFG/ESP-IDF-Pruebas/PruebaDefinitiva/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
